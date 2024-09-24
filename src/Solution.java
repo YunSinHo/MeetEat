e346acb76403a7737cd8a45271e916f2c316e4a0
yunsinho <mallrege@naver.com>
2024-09-24 16:24:40 +0900

    테스트

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(4, 5, new int[] { 1, 0, 3, 1, 2 }, new int[] { 0, 3, 0, 4, 0 }));
+    }
+
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int delIdx = n - 1;
+        int pickIdx = n - 1;
+        while(delIdx >= 0) {
+            
+        }
+        return answer;
+    }
+}
