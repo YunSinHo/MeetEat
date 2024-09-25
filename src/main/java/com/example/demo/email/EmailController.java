dec624e439862979a2e2332de5c185e6d087ca5e
yunsinho <mallrege@naver.com>
2024-09-25 17:09:51 +0900

    email service

--- src/main/java/com/example/demo/email/EmailController.java
+++ src/main/java/com/example/demo/email/EmailController.java
@@ -1,34 +1,18 @@
 package com.example.demo.email;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
 public class EmailController {
 
     @Autowired
     private EmailService emailService;
 
-    // @PostMapping("/signup")
-    // public ResponseEntity<String> signUp(@RequestParam String email) {
-    //     String verificationCode = VerificationCodeGenerator.generateVerificationCode();
-    //     try {
-    //         emailService.sendVerificationEmail(email, verificationCode);
-    //         return ResponseEntity.ok("Verification email sent to " + email);
-    //     } catch (Exception e) {
-    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-    //                             .body("Failed to send email: " + e.getMessage());
-    //     }
-    // }
     @GetMapping("/send-email")
     public String sendEmail() {
-        emailService.sendSimpleMessage();
-        return "redirect:/user/login";
+        emailService.sendSimpleEmail("mallrege@naver.com", "Test Subject", "Test Email Content");
+        return "Email sent successfully!";
     }
-    
-    
 }
+
